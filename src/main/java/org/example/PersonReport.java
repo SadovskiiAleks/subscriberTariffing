2e6d59494ecbec108657f9882c02e2e268a4854d
SadovskiiAleks <alekssadovskij@yandex.ru>
2023-03-25 21:44:32 +0300

    add work 25.03.2023 before 20:00

--- src/main/java/org/example/PersonReport.java
+++ src/main/java/org/example/PersonReport.java
@@ -1,36 +1,98 @@
 package org.example;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 
 public class PersonReport {
-    private String number = "";
-//    private String typeOfColl = "";
-//    private String startTimeAndDateOfColl = "";
-//    private String endTimeAndDateOfColl = "";
-//    private String typeOfRate = "";
-    private LinkedList<String[]> list = new LinkedList();
+    private LinkedList<Coll> list = new LinkedList();
+    private long timeRate;
+    private int rate = 0;
+    private double allCost = 0;
 
-    private String[] TimeAndDateOfColl = new String[5];
+    public PersonReport(int rate) {
+        this.rate = rate;
+        switch (rate) {
+            case (06):
+                timeRate = 300*60;
+                allCost = 100;
+                break;
+            case (03):
+                timeRate = 0L;
+                break;
+            case (11):
+                timeRate = 100*60;
+                break;
+        }
+    }
 
-    // Устнановить тариф
+    public void addToList(Coll coll) {
+        //Получить время звонка в
+        coll.setPriceCall(findCost(coll));
 
-    public void addToList(String[] arrayOfString) {
-
-
-        list.add(arrayOfString);
-
+        list.add(coll);
         // после каждого добавления элемента провести расчет для каждой минуты по тарифному плану и добавить в строку 6
 
+    }
+
+    private double findCost(Coll coll) {
+        double cost = 0;
+        switch (rate) {
+            case (06):
+                cost = costRate06(coll);
+                break;
+            case (03):
+                cost = costRate03(coll);
+                break;
+            case (11):
+                cost = costRate11(coll);
+                break;
+        }
+        return cost;
+    }
+
+    private double costRate06(Coll coll) {
+        if (timeRate - coll.getDifference() >= 0) {
+            timeRate -= coll.getDifference();
+            return 0;
+        } else {
+            allCost += (double)coll.getDifference()/60 * 1 + (double)timeRate/60 * 1;
+            timeRate = 0;
+            return (double)coll.getDifference()/60 * 1;
+        }
+    }
+
+    private double costRate03(Coll coll) {
+        allCost += (double)coll.getDifference()/60 * 1.5;
+        return (double)coll.getDifference()/60 * 1.5;
+    }
+
+    private double costRate11(Coll coll) {
+        // Входящие - бесплатно, исходящие - первые 100 минут по 0.5р/минута, после по тарифу "поминутный".
+        if (coll.getInOutColl() == 02) {
+            // бесплатно
+            return 0;
+        } if (coll.getInOutColl() == 01) {
+            //Платно
+            if (timeRate - coll.getDifference() >= 0) {
+                timeRate -= coll.getDifference();
+                allCost += (double)coll.getDifference()/60 * 0.5;
+                return (double)coll.getDifference()/60 * 0.5;
+            } else {
+                allCost += (double)coll.getDifference()/60 * 1.5 + (double)timeRate/60 * 1.5;
+                timeRate = 0;
+                return (double)coll.getDifference()/60 * 1.5;
+            }
+        }
+        return 0;
     }
 
 
     @Override
     public String toString() {
-        for (String[] s :list){
-            for (String sed: s){
-                System.out.print(sed + ",");
-            }
-            System.out.println();
+        for (Coll c : list) {
+            c.toString();
         }
         return null;
     }
--- src/main/java/org/example/ReaderCDR.java
+++ src/main/java/org/example/ReaderCDR.java
@@ -11,25 +11,26 @@ public class ReaderCDR {
     private BufferedReader br = null;
     private String line = "";
     private int checkLine = 0;
-    private HashMap<String, PersonReport> hashMap = new HashMap<>();
+    private HashMap<Long, PersonReport> hashMap = new HashMap<>();
 
     public ReaderCDR readFileToClass(String fileWay, String splitBy) {
         try {
             br = new BufferedReader(new FileReader(fileWay));
             while ((line = br.readLine()) != null) {
                 String[] arrayFromFile = line.split(splitBy);
-                String[] parsingArray = parsing(arrayFromFile);
+                CollBuilder collBuilder = new CollBuilder(arrayFromFile);
+                Coll coll = collBuilder.build();
 
-                String number = parsingArray[0];
+                Long number = coll.getNumber();
 
                 if (hashMap.containsKey(number)) {
                     // нужно получить по ключю объект и добавить элемент в массив
-                    hashMap.get(number).addToList(parsingArray);
+                    hashMap.get(number).addToList(coll);
 
                 } else {
                     // Нужно создать объект и добавить элемент
-                    PersonReport personReport = new PersonReport();
-                    personReport.addToList(parsingArray);
+                    PersonReport personReport = new PersonReport(coll.getRate());
+                    personReport.addToList(coll);
                     hashMap.put(number, personReport);
                 }
 
@@ -52,61 +53,7 @@ public class ReaderCDR {
         return this;
     }
 
-    public String[] parsing(String[] arrayFromFile) {
-        String[] parsingArray = new String[arrayFromFile.length];
-        Long maxLong = 0L;
-        // 0 Номер абонента
-        // 1 Тип вызова
-        // 2 Дата и время начала звонка (YYYYMMDDHH24MMSS)
-        // 3 Дата и время окончания звонка
-        // 4 Тип тарфа (см. ниже)
 
-        for (String s : arrayFromFile) {
-            //Проверить каждый элемент
-            switch (s.length()) {
-                case (2):
-                    // Входящий исходящий //01, 02
-                    // Тип тарифа //11, 06, 03
-                    switch (s) {
-                        case ("01"):
-                        case ("02"):
-                            parsingArray[1] = s;
-                            break;
-                        case ("11"):
-                        case ("06"):
-                        case ("03"):
-                            parsingArray[4] = s;
-                            break;
-                    }
-                    break;
-                case (11):
-                    //Номер;
-                    //73734435243,
-                    parsingArray[0] = s;
-                    break;
-                case (14):
-                    //Код1;
-                    //  2023-07-25 14.14.48, 2023-07-25 14.21.10
-                    if (maxLong == 0L){
-                        maxLong = Long.parseLong(s);
-                    } else {
-                        if (maxLong > Long.parseLong(s)){
-                            parsingArray[2] = s;
-                            parsingArray[3] = maxLong.toString();
-                        } else {
-                            parsingArray[2] = maxLong.toString();
-                            parsingArray[3] = s;
-                        }
-                    }
-                    break;
-                default:
-                    //Код по умолчанию;
-                    break;
-            }
-        }
-
-        return parsingArray;
-    }
 
     @Override
     public String toString() {
