0d677a72d17dcca302bb2abf281cebc7cac39552
SadovskiiAleks <alekssadovskij@yandex.ru>
2023-03-26 23:00:09 +0300

    add buildJarOnJDK11.jar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,34 @@
 package org.example;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // Считать строки файл
-        ReaderCDR readerCDR = new ReaderCDR().readFileToClass("src/main/resources/task/cdr.txt", ", ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sad way to cdr.txt");
+        System.out.println();
+        String fileWay = scanner.nextLine();
+        ReaderCDR readerCDR = new ReaderCDR().readFileToClass(fileWay, ", ");
 
         //Сохранить отчет
-        new SafeToFile(readerCDR.getHashMap(), "reports/");
+        System.out.println();
+        System.out.println("Sad way to save reports");
+        System.out.println();
+        String wayReports = scanner.nextLine();
+        //создать файл
+
+        File directory = new File(wayReports+"\\reports\\");
+
+        if (directory.mkdir()){
+            System.out.println("Reports is created!");
+        }
+        else{
+            System.out.println("reports already exists.");
+        }
+        new SafeToFile(readerCDR.getHashMap(), wayReports + "\\reports\\");
+        System.out.println("Nays day!");
     }
 }
